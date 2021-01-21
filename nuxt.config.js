515bc6fdbd790e632945bbc2b9c2f04f9c657c9b
knight2711 <wattanasatta2711@gmail.com>
2021-01-21 13:37:52 +0700

    script

--- nuxt.config.js
+++ nuxt.config.js
@@ -9,6 +9,9 @@ export default {
    ** See https://nuxtjs.org/api/configuration-target
    */
   target: 'static',
+  router: {
+    base: '/Web_app/'
+  },
   /*
    ** Headers of the page
    ** See https://nuxtjs.org/api/configuration-head

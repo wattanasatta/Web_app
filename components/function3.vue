d40fb9ad0ce4b4896197495fefe377e780254a3a
knight2711 <wattanasatta2711@gmail.com>
2021-01-21 12:17:56 +0700

    new test

--- components/function3.vue
+++ components/function3.vue
@@ -2,11 +2,25 @@
   <div style="width:100%; background:RGB(203,213,224);">
     <h3 class="pt-2">Test Function 3</h3>
     <div>case1: {{a1}}</div>
-    <div>{{findMax2(a1) ?findMax2(a1) : 'null' }}</div>
+    <div>{{findMax2(a1) }}</div>
     <div>case2: {{a2}}</div>
     <div>{{findMax2(a2)}}</div>
     <div>case3: {{a3}}</div>
     <div>{{findMax2(a3)}}</div>
+    <div>case4: {{a4}}</div>
+    <div>{{findMax2(a4)}}</div>
+    <div>case5: {{a5}}</div>
+    <div>{{findMax2(a5)}}</div>
+    <div>case6: {{a6}}</div>
+    <div>{{findMax2(a6)}}</div>
+    <div>case7: {{a7}}</div>
+    <div>{{findMax2(a7)}}</div>
+    <div>case8: {{a8}}</div>
+    <div>{{findMax2(a8)}}</div>
+    <div>case9: {{a9}}</div>
+    <div>{{findMax2(a9)}}</div>
+    <div>case10: {{a10}}</div>
+    <div>{{findMax2(a10)}}</div>
   </div>
 </template>
 
@@ -21,30 +35,34 @@ export default {
     return {
       a1: [],
       a2: [1],
-      a3: [1, 2, 5, 4, 5, 10]
+      a3: [1,2,3], // 2
+      a4: [1,1], // 1
+      a5: [1,2,3,4,5,6], // 5
+      a6: [1,5,3,2,5,10], // 5
+      a7: [100,5,3,2,99], // 99
+      a8: [35,5,3,2,5,100], // 35
+      a9: [1,5,101,2,5,10], // 10
+      a10: [10,10,9], // 9
     };
   },
   methods: {
     findMax2(array) {
-      var sorted = [];
-      if (!array) return null;
+      var sorted = array.slice(0)
+      if (array.length < 1) return 'null';
       else if (array.length == 1) return array[0];
-      // array.sort((a, b) => a - b);
       else {
+        sorted.sort((a, b) => a - b)
         var maxArray = [];
-        // var index = array.length;
-        var max = array[0];
+        var max = sorted[0];
         maxArray.push(max);
-        for (let index = 0; index < array.length; index++) {
-          const element = array[index];
+        for (let index = 1; index < sorted.length; index++) {
+          const element = sorted[index];
           if (element > max) {
             max = element;
             maxArray.push(max);
           }
         }
-        // var max2 = array[index - 2];
-
-        return maxArray[maxArray.length - 2];
+        return maxArray.length > 1 ? maxArray[maxArray.length - 2] : maxArray[0];
       }
     }
   }
